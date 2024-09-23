01938ad7bf8dfaa7ffb21a1a08a10dd7edc21e1d
Eric Appiah Adjei <adjei.net@Erics-MacBook-Pro.local>
2024-09-23 13:09:33 -0400

    corrected ShippingCalculator

--- src/ShippingCalculator.java
+++ src/ShippingCalculator.java
@@ -1,15 +1,21 @@
+
 public class ShippingCalculator {
     public double calculateShippingCost(double weight, double distance) {
         double cost;
+        private static final double WEIGHT_UNDER_FIVE = 0.50
+        private static final double WEIGHT_UNDER_TEN = 0.75
+        private static final double WEIGHT_UNDER_TWENTY = 1.00
+        private static final double WEIGHT_OVER_TWENTY = 1.50
+
 
         if (weight <= 5) {
-            cost = distance * 0.50; // $0.50 per mile for weights up to 5 lbs
+            cost = distance * WEIGHT_UNDER_FIVE; // $0.50 per mile for weights up to 5 lbs
         } else if (weight <= 10) {
-            cost = distance * 0.75; // $0.75 per mile for weights up to 10 lbs
+            cost = distance * WEIGHT_UNDER_TEN; // $0.75 per mile for weights up to 10 lbs
         } else if (weight <= 20) {
-            cost = distance * 1.00; // $1.00 per mile for weights up to 20 lbs
+            cost = distance * WEIGHT_UNDER_TWENTY; // $1.00 per mile for weights up to 20 lbs
         } else {
-            cost = distance * 1.50; // $1.50 per mile for weights over 20 lbs
+            cost = distance * WEIGHT_OVER_TWENTY; // $1.50 per mile for weights over 20 lbs
         }
 
         return cost;
